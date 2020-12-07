cee9e0e6954c5d5e0b8bed591201d7c602bc117d
DDH <ddhoang1993@gmail.com>
2020-12-07 00:57:35 -0800

    updated services section

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Container, Row, Col, Card, Accordion } from 'react-bootstrap';
-import image3 from '../../img/services/Security-04.png';
-import image4 from '../../img/services/StategicConslt-03.png';
 import './style.css';
+import { Container, Row, Col, Card, Accordion } from 'react-bootstrap';
+
+import image1 from '../../img/services/security-05.png';
+import image2 from '../../img/services/consulting-05.png';
+import image3 from '../../img/services/cost-05.png';
+import image4 from '../../img/services/DevOps-05.png';
+import image5 from '../../img/services/migrate-05.png';
+import image6 from '../../img/services/build-05.png';
 
 const Cards = () => {
     return (
@@ -11,17 +16,18 @@ const Cards = () => {
                     <Row>
                         <Col lg className='ml-5 mb-3'>
                             <Accordion>
-                                <Card id='card1' style={{ width: '18rem' }} className='text-center'>
+                                <Card id='card'>
                                     <Accordion.Toggle as={Card.Img} eventKey='1'
                                         alt='AWS'
-                                        src={image4}
+                                        src={image1}
                                         height='150px'
                                         variant='top'
                                     />
                                     <Card.Body>
-                                        <Card.Title>Migrate to AWS</Card.Title>
+                                        <Card.Title className='card-title' style={{fontSize: '23px'}}>Security & Compliance</Card.Title>
+                                        <p className='line font-weight-bold'>_______</p>
                                         <Accordion.Collapse eventKey='1'>
-                                            <Card.Text>Show you the best and most efficient way to migrate your workloads to AWS</Card.Text>
+                                            <Card.Text style={{fontSize: '16px'}}>Ensure that your infrastructure follows AWS Security Best Practices like Netflix, Twitch, and LinkedIn</Card.Text>
                                         </Accordion.Collapse>
                                     </Card.Body>
                                 </Card>
@@ -29,17 +35,18 @@ const Cards = () => {
                         </Col>
                         <Col lg className='ml-5 mb-3'>
                             <Accordion>
-                                <Card id='card2' style={{ width: '18rem' }} className='text-center'>
+                                <Card id='card'>
                                     <Accordion.Toggle as={Card.Img} eventKey='2' 
                                         alt='build'
-                                        src={image3}
+                                        src={image2}
                                         height='150px'
                                         variant='top'  
                                     />
                                     <Card.Body>
-                                        <Card.Title>Build Application</Card.Title>
+                                        <Card.Title className='card-title' style={{fontSize: '23px'}}>Strategic Consulting</Card.Title>
+                                        <p className='line font-weight-bold'>_______</p>
                                         <Accordion.Collapse eventKey='2'>
-                                            <Card.Text>Build highly scalable applications based on serverless architecture</Card.Text>
+                                            <Card.Text style={{fontSize: '16px'}}>Offer architectural guidance on how to start and optimize your AWS projects from start to finish</Card.Text>
                                         </Accordion.Collapse>
                                     </Card.Body>
                                 </Card>
@@ -47,7 +54,7 @@ const Cards = () => {
                         </Col>
                         <Col lg className='ml-5 mb-3'>
                             <Accordion>
-                                <Card id='card3' style={{ width: '18rem' }} className='text-center'>
+                                <Card id='card'>
                                     <Accordion.Toggle as={Card.Img} eventKey='3'
                                         alt='security'
                                         src={image3}
@@ -55,9 +62,10 @@ const Cards = () => {
                                         variant='top'
                                     />
                                     <Card.Body>
-                                        <Card.Title>Security</Card.Title>
+                                        <Card.Title className='card-title' style={{fontSize: '23px'}}>Cost</Card.Title>
+                                        <p className='line font-weight-bold'>_______</p>
                                         <Accordion.Collapse eventKey='3'>
-                                            <Card.Text>Ensure that your infrastructure follows AWS Security Best Practices like Netflix, Twitch, and LinkedIn</Card.Text>
+                                            <Card.Text style={{fontSize: '16px'}}>Significantly reduce your AWS bill (up to 50%) by optimizing your environment for costs</Card.Text>
                                         </Accordion.Collapse>
                                     </Card.Body>
                                 </Card>
@@ -67,7 +75,7 @@ const Cards = () => {
                     <Row>
                         <Col lg className='ml-5 mb-3'>
                             <Accordion>
-                                <Card id='card4' style={{ width: '18rem' }} className='text-center'>
+                                <Card id='card'>
                                     <Accordion.Toggle as={Card.Img} eventKey='4'
                                         alt='strategic'
                                         src={image4}
@@ -75,9 +83,10 @@ const Cards = () => {
                                         variant='top'  
                                     />
                                     <Card.Body>
-                                        <Card.Title>Strategic Consulting</Card.Title>
+                                        <Card.Title className='card-title' style={{fontSize: '23px'}}>DevOps</Card.Title>
+                                        <p className='line font-weight-bold'>_______</p>
                                         <Accordion.Collapse eventKey='4'>
-                                            <Card.Text>Offer architectural guidance on how to start and optimize your AWS projects from start to finish</Card.Text>
+                                            <Card.Text style={{fontSize: '16px'}}>Give you hands-on help with automating the deployment, testing, and monitoring of your services</Card.Text>
                                         </Accordion.Collapse>
                                     </Card.Body>
                                 </Card>
@@ -85,17 +94,18 @@ const Cards = () => {
                         </Col>
                         <Col lg className='ml-5 mb-3'>
                             <Accordion>
-                                <Card id='card5' style={{ width: '18rem' }} className='text-center'>
+                                <Card id='card'>
                                     <Accordion.Toggle as={Card.Img} eventKey='5' 
                                         alt='cost'
-                                        src={image4}
+                                        src={image5}
                                         height='150px'
                                         variant='top'  
                                     />
                                     <Card.Body>
-                                        <Card.Title>Cost</Card.Title>
+                                        <Card.Title className='card-title' style={{fontSize: '23px'}}>Migrate to AWS</Card.Title>
+                                        <p className='line font-weight-bold'>_______</p>
                                         <Accordion.Collapse eventKey='5'>
-                                            <Card.Text>Significantly reduce your AWS bill (up to 50%) by optimizing your environment for costs</Card.Text>
+                                            <Card.Text style={{fontSize: '16px'}}>Show you the best and most efficient way to migrate your workloads to AWS</Card.Text>
                                         </Accordion.Collapse>
                                     </Card.Body>
                                 </Card>
@@ -103,17 +113,18 @@ const Cards = () => {
                         </Col>
                         <Col lg className='ml-5 mb-3'>
                             <Accordion>
-                                <Card id='card6' style={{ width: '18rem' }} className='text-center'>
+                                <Card id='card'>
                                     <Accordion.Toggle as={Card.Img} eventKey='6' 
                                         alt='devops'
-                                        src={image3}
+                                        src={image6}
                                         height='150px'
                                         variant='top'  
                                     />
                                     <Card.Body>
-                                        <Card.Title>DevOps</Card.Title>
+                                        <Card.Title className='card-title' style={{fontSize: '23px'}}>Build Application</Card.Title>
+                                        <p className='line font-weight-bold'>_______</p>
                                         <Accordion.Collapse eventKey='6'>
-                                            <Card.Text>Give you hands-on help with automating the deployment, testing, and monitoring of your servives</Card.Text>
+                                            <Card.Text style={{fontSize: '16px'}}>Build highly scalable applications based on serverless architecture</Card.Text>
                                         </Accordion.Collapse>
                                     </Card.Body>
                                 </Card>
